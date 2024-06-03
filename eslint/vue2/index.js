46c5bcea63734574b66906b39c71a3b24971ca2b
feijie.zheng <feijie.zheng@lkcoffee.com>
2024-06-03 13:46:58 +0800

    feat(eslint): vue引用组件用短横线法

--- eslint/vue2/index.js
+++ eslint/vue2/index.js
@@ -3,16 +3,10 @@
 module.exports = {
   extends: [ 'plugin:vue/recommended' ],
   rules: {
-    // 对 Vue 属性换行的检查
-    'vue/max-attributes-per-line': 0,
-    // 在单行元素的内容之前和之后换行
-    'vue/singleline-html-element-content-newline': 0,
-    // 强制元素自闭标签
-    'vue/html-self-closing': 0,
     // 对不可变属性的检查
     'vue/no-mutating-props': [ 2, { "shallowOnly": true } ],
-    // 对 HTML 缩进的检查
-    'vue/html-indent': 0,
+    // 模板中引用组件名用短横线法（而不是驼峰法）
+    'vue/component-name-in-template-casing': [ 1, "kebab-case", { registeredComponentsOnly: false } ],
     // 禁止使用v-html以防止XSS攻击
     'vue/no-v-html': 1,
     // 要求组件名称始终为多字
@@ -24,9 +18,18 @@ module.exports = {
     // 单文件组件中是否存在未使用的组件
     'vue/no-unused-components': 1,
 
+    // 对 Vue 属性换行的检查
+    'vue/max-attributes-per-line': 0,
+    // 在单行元素的内容之前和之后换行
+    'vue/singleline-html-element-content-newline': 0,
+    // 强制元素自闭标签
+    'vue/html-self-closing': 0,
+    // 对 HTML 缩进的检查
+    'vue/html-indent': 0,
+
     // 即将被eslint去除的规则
     "space-before-function-paren": 0,
-    "space-in-parens": 0
+    "space-in-parens": 0,
   },
   parserOptions: {
     ecmaVersion: 'latest',
